dab3d1098672daf08844bb431fd5c07a923556f4
Abel <abel.liu.china@outlook.com>
2016-03-19 20:24:09 +0800

    Create shi-ci.com.user.js

---
+++ shi-ci.com.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         shi-ci.com.user.js
+// @namespace    http://abelliu.com
+// @version      1.0
+// @description  change article style.
+// @author       abel
+// @require      https://code.jquery.com/jquery-2.2.2.min.js
+// @import       http*://www.shi-ci.com/*
+// @grant        none
+// ==/UserScript==
+
+$(function() {
+    var e = $("div.poem-read p");
+    e.css("font-size","20px");
+    e.css("line-height","1");
+    e.css("letter-spacing","0");
+
+    $("div.x-box").css("padding","3px 10px");
+    $("div.x-box-top").css("padding","5px");
+
+    $("body").css("background-image","url(a.jpg)");
+});
